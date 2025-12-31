6fe0a3b72ee90ec1cf61f207fe1c64d04d1a49d3
ederinedirnet-sketch <ederinedir.net@gmail.com>
2025-12-31 13:40:48 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,10 @@ export default async function Home() {
   const data = snap.exists() ? snap.data() : null;
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white dark:bg-black">
-        <h1 className="text-3xl font-bold text-black dark:text-white">
-          {data ? data.title : "Veri bulunamadı"}
-        </h1>
-      </main>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <h1 className="text-4xl font-bold text-black dark:text-white">
+        {data ? data.title : "Veri bulunamadı"}
+      </h1>
     </div>
   );
 }
