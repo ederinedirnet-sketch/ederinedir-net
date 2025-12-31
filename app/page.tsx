dafdd34a9f6e1de71c3e3327d3c3ca632c9668ea
ederinedirnet-sketch <ederinedir.net@gmail.com>
2025-12-31 13:38:02 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { app } from "./../lib/firebase";
-
-import Image from "next/image";
+import { app } from "../lib/firebase";
 
 export default async function Home() {
   const db = getFirestore(app);
@@ -11,12 +9,10 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white dark:bg-black">
         <h1 className="text-3xl font-bold text-black dark:text-white">
           {data ? data.title : "Veri bulunamadı"}
         </h1>
-
       </main>
     </div>
   );
